59b67b867e6806cab7120cba2e38d85c3cabfa44
iamjjanga-ouo <namils147@kakao.com>
2020-12-28 21:13:25 +0900

    add dynamodb code

--- init/init.tf
+++ init/init.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "ap-northeast-2"
-  version = "~> 3.22.0"
 }
 
 resource "aws_s3_bucket" "tfstate" {
@@ -8,5 +7,15 @@ resource "aws_s3_bucket" "tfstate" {
 
   versioning {
     enabled = true # Prevent from deleting tfstate file
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
