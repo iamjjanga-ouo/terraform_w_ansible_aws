93148db92f51130ff1ea8c53f8cd15f05899383b
iamjjanga-ouo <namils147@kakao.com>
2020-12-31 20:34:45 +0900

    init ALB security group, change web_sg inbound rule : source 'anywhere -> alb_sg'

--- vpc/my_vpc.tf
+++ vpc/my_vpc.tf
@@ -119,27 +119,43 @@ resource "aws_route_table_association" "db_private" {
 }
 
 ##################
-# Security Group
+# Security Groups
 ##################
 resource "aws_security_group" "web_sg" {
   name = "web_sg"
   description = "Allow HTTP, HTTPS"
   vpc_id = aws_vpc.main.id
 
-  ingress {
-    description = "HTTP for WEB"
+//  ingress {
+//    description = "HTTP for WEB"
+//    from_port = 80
+//    to_port = 80
+//    protocol = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//
+//  ingress {
+//    description = "HTTPS for WEB"
+//    from_port = 443
+//    to_port = 443
+//    protocol = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+
+    ingress {
+    description = "HTTP from ALB"
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   ingress {
-    description = "HTTPS for WEB"
+    description = "HTTPS from ALB"
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   ingress {
@@ -159,5 +175,40 @@ resource "aws_security_group" "web_sg" {
 
   tags = {
     Name = "web_sg"
+    Environment = "development"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name = "alb_sg"
+  description = "Allow HTTP, HTTPS"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+    Environment = "development"
   }
 }
