391cc6b656aa51268915804f5c6fed77d923e76a
iamjjanga-ouo <namils147@kakao.com>
2020-12-31 20:35:14 +0900

    add target group, listener

--- vpc/elb.tf
+++ vpc/elb.tf
@@ -1,28 +1,57 @@
-resource "aws_elb" "ext_elb" {
-  name = "ext-elb"
+resource "aws_lb" "ex_alb" {
+  name               = "${var.vpc_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public.*.id
 
-  listener {
-    instance_port = 8000
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "${var.vpc_name}-alb"
+    Environment = "development"
   }
+}
 
-  listener {
-    instance_port = 8000
-    instance_protocol = "http"
-    lb_port = 443
-    lb_protocol = "https"
-    #ssl_certificate_id = ""
+resource "aws_lb_target_group" "ex_alb_target" {
+  name     = "${var.vpc_name}-alb-target-group"
+  port     = 80                                 # ALB-WEB doesn't need 443(HTTP) because of offloading
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group_attachment" "ex_alb_target_attach" {
+  count = 2
+  target_group_arn = aws_lb_target_group.ex_alb_target.arn
+  target_id        = element(aws_instance.web.*.id, count.index)
+  port             = 80
+}
+
+resource "aws_lb_listener" "ex_alb_listener_http" {
+  load_balancer_arn = aws_lb.ex_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
+}
 
-  health_check {
-    healthy_threshold = 2
-    interval = 30
-    target = "HTTP:8000/"
-    timeout = 3
-    unhealthy_threshold = 2
+resource "aws_lb_listener" "ex_alb_listener_https" {
+  load_balancer_arn = aws_lb.ex_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.ssl_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ex_alb_target.arn
   }
-
-  instances = aws_instance.web.*.id
 }
