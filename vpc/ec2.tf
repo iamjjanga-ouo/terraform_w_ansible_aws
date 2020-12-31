afec83c1828211014c171c5347d4b2369137ad9d
iamjjanga-ouo <namils147@kakao.com>
2020-12-31 20:36:56 +0900

    add index number in index.html that can be distinguished for load-balancing

--- vpc/ec2.tf
+++ vpc/ec2.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "web" {
           #!/bin/bash
           sudo amazon-linux-extras install nginx1 -y
           sudo systemctl enable --now nginx
-          echo "<h1>Deployed via Terraform</h1>" | sudo tee /usr/share/nginx/html/index.html
+          echo "<h1>Deployed via Terraform-${count.index}</h1>" | sudo tee /usr/share/nginx/html/index.html
   EOF
 
   tags = {
