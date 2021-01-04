591bee632ab13a9c50d8e180cee3a7b7edf9997c
iamjjanga-ouo <namils147@kakao.com>
2021-01-04 17:41:42 +0900

    Edit Package manger name for rhel

--- vpc/install_nginx_php.sh
+++ vpc/install_nginx_php.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo amazon-linux-extras install nginx1 -y
+sudo dnf install nginx -y
 sudo systemctl enable --now nginx
 echo "<h1>Deployed via Terraform-${count.index}</h1>" | sudo tee /usr/share/nginx/html/index.html
