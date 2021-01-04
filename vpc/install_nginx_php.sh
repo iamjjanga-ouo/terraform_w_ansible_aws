119a9624f07bfacaa96c45e915b64c28c90c5e43
iamjjanga-ouo <namils147@kakao.com>
2021-01-04 17:49:09 +0900

    Can't using 'count' variable in file.

--- vpc/install_nginx_php.sh
+++ vpc/install_nginx_php.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo dnf install nginx -y
 sudo systemctl enable --now nginx
-echo "<h1>Deployed via Terraform-${count.index}</h1>" | sudo tee /usr/share/nginx/html/index.html
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /usr/share/nginx/html/index.html
