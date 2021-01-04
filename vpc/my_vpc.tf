27fc1776d65d83fc98fced3fe691ef1a0a4e0b92
iamjjanga-ouo <namils147@kakao.com>
2021-01-04 16:43:50 +0900

    create security group 'db_sg'

--- vpc/my_vpc.tf
+++ vpc/my_vpc.tf
@@ -126,22 +126,6 @@ resource "aws_security_group" "web_sg" {
   description = "Allow HTTP, HTTPS"
   vpc_id = aws_vpc.main.id
 
-//  ingress {
-//    description = "HTTP for WEB"
-//    from_port = 80
-//    to_port = 80
-//    protocol = "tcp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//
-//  ingress {
-//    description = "HTTPS for WEB"
-//    from_port = 443
-//    to_port = 443
-//    protocol = "tcp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-
     ingress {
     description = "HTTP from ALB"
     from_port = 80
@@ -209,6 +193,32 @@ resource "aws_security_group" "alb_sg" {
 
   tags = {
     Name = "alb_sg"
+    Environment = "development"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name = "db_sg"
+  description = "Allow from ec2"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "MySQL"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db_sg"
     Environment = "development"
   }
 }
