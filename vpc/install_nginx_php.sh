e863350aa5852c418de64401a5e99813bc56fa75
iamjjanga-ouo <namils147@kakao.com>
2021-01-05 23:03:24 +0900

    edit install_nginx_php.sh

--- vpc/install_nginx_php.sh
+++ vpc/install_nginx_php.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo dnf update -y
 sudo dnf install nginx -y
 sudo systemctl enable --now nginx
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+# sudo dnf module list php
+sudo dnf module enable php:remi-7.4 -y
+sudo dnf install php php-cli php-common -y
+sudo systemctl enable --now php-fpm
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /usr/share/nginx/html/index.html
