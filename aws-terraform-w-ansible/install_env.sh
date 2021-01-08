3f6e70541b7a1d071d334698506fb02463224dc1
iamjjanga-ouo <namils147@kakao.com>
2021-01-08 17:16:33 +0900

    Change passwordAuthentication no -> yes

--- aws-terraform-w-ansible/install_env.sh
+++ aws-terraform-w-ansible/install_env.sh
@@ -2,7 +2,15 @@
 sudo dnf update -y
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 sudo dnf install python3 -y
-sudo dnf install ansible -y
+
+# bash_ssh_4_CentOS
+now=$(date +"%m_%d_%Y")
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup
+sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# set ec2-user password
+echo "dkagh1." | sudo passwd --stdin ec2-user
 
 #sudo dnf update -y
 #sudo dnf install nginx -y
