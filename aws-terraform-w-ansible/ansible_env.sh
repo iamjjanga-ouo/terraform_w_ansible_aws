b6b216eba493fff78af7f682e6ac75373a443253
iamjjanga-ouo <namils147@kakao.com>
2021-01-13 22:25:59 +0900

    Shell file typo

--- aws-terraform-w-ansible/ansible_env.sh
+++ aws-terraform-w-ansible/ansible_env.sh
@@ -11,7 +11,11 @@ echo "alias ans='ansible'" >> ~/.bashrc
 echo "alias anp='ansible-playbook'" >> ~/.bashrc
 source /home/ec2-user/.bashrc
 
+# bash_ssh_4_CentOS
+now=$(date +"%m_%d_%Y")
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_"$now".backup
+
 # ssh timeout
 sudo sed -i -e 's/#ClientAliveInterval 0/ClientAliveInterval 10m/g' /etc/ssh/sshd_config
-sudo sed -i -e 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/g' /etc/ssh/sshd_config
+sudo sed -i -e 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/g' /etc/ssh/sshd_configz
 sudo systemctl restart sshd
--- aws-terraform-w-ansible/web_env.sh
+++ aws-terraform-w-ansible/web_env.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-sudo dnf update -y
-sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-sudo dnf install python3 -y
 
 # bash_ssh_4_CentOS
 now=$(date +"%m_%d_%Y")
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_"$now".backup
 sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# ssh timeout
+sudo sed -i -e 's/#ClientAliveInterval 0/ClientAliveInterval 10m/g' /etc/ssh/sshd_config
+sudo sed -i -e 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
 # set ec2-user password
 echo "dkagh1." | sudo passwd --stdin ec2-user
 
-# ssh timeout
-sudo sed -i -e 's/#ClientAliveInterval 0/ClientAliveInterval 10m/g' /etc/ssh/sshd_config
-sudo sed -i -e 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+#sudo dnf update -y
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo dnf install python3 -y
 
 
 
