4cb5f2c9cf06a2ca13b638c86f1e8cf270d429b1
iamjjanga-ouo <namils147@kakao.com>
2021-01-19 22:35:42 +0900

    Uncomment DB blocks

--- aws-terraform-w-ansible/main.tf
+++ aws-terraform-w-ansible/main.tf
@@ -59,33 +59,33 @@ resource "aws_route_table_association" "public" {
 ####################
 ## Private Subnet x 2
 ####################
-resource "aws_subnet" "private" {
-  count = length(var.availability_zones)
-  vpc_id = aws_vpc.main.id
-
-  cidr_block = "10.${var.cidr_numeral}.${var.cidr_numeral_private[count.index]}.0/20"
-  availability_zone = element(var.availability_zones, count.index)
-  
-  tags = {
-    Name = "${var.vpc_name}_private_${count.index}"
-    Network = "private"
-  }
-}
-
-# Private route table
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.vpc_name}_private_rt"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  count = length(var.availability_zones)
-  route_table_id = aws_route_table.private.id
-  subnet_id = element(aws_subnet.private.*.id, count.index)
-}
+//resource "aws_subnet" "private" {
+//  count = length(var.availability_zones)
+//  vpc_id = aws_vpc.main.id
+//
+//  cidr_block = "10.${var.cidr_numeral}.${var.cidr_numeral_private[count.index]}.0/20"
+//  availability_zone = element(var.availability_zones, count.index)
+//
+//  tags = {
+//    Name = "${var.vpc_name}_private_${count.index}"
+//    Network = "private"
+//  }
+//}
+//
+//# Private route table
+//resource "aws_route_table" "private" {
+//  vpc_id = aws_vpc.main.id
+//
+//  tags = {
+//    Name = "${var.vpc_name}_private_rt"
+//  }
+//}
+//
+//resource "aws_route_table_association" "private" {
+//  count = length(var.availability_zones)
+//  route_table_id = aws_route_table.private.id
+//  subnet_id = element(aws_subnet.private.*.id, count.index)
+//}
 
 ####################
 ## DB Private Subnet x 2
@@ -162,6 +162,10 @@ resource "aws_security_group" "web_sg" {
     Environment = "development"
   }
 }
+
+###
+# was sg 추가
+###
 
 resource "aws_security_group" "alb_sg" {
   name = "alb_sg"
--- aws-terraform-w-ansible/rds.tf
+++ aws-terraform-w-ansible/rds.tf
@@ -1,26 +1,27 @@
-//resource "aws_db_instance" "rds" {
-//  count = length(var.availability_zones)
-//  allocated_storage    = 20
-//  storage_type         = "gp2"
-//  engine               = "mysql"
-//  engine_version       = "5.7"
-//  instance_class       = "db.t2.micro"
-//  name                 = "mydb"
-//  username             = "admin"
-//  password             = "dkagh1.."
-//  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.id
-//  availability_zone = element(var.availability_zones, count.index)
-//
-//  # ERROR : DB Instance FinalSnapshotIdentifier is required when a final snapshot is required -> https://github.com/hashicorp/terraform-provider-aws/issues/2588
-//  skip_final_snapshot = true
-//}
-//
-//resource "aws_db_subnet_group" "rds_subnet_group" {
-//  #count = length(var.availability_zones)
-//  name       = "rds_sg"
-//  #subnet_ids = list(aws_subnet.db_private[count.index].id)
-//  subnet_ids = aws_subnet.db_private.*.id
-//  tags = {
-//    Name = "${var.vpc_name}_rds_sg"
-//  }
-//}
+resource "aws_db_instance" "my_db" {
+  count = length(var.availability_zones)
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = var.my_db_password
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.id
+  availability_zone = element(var.availability_zones, count.index)
+
+  # ERROR : DB Instance FinalSnapshotIdentifier is required when a final snapshot is required -> https://github.com/hashicorp/terraform-provider-aws/issues/2588
+  skip_final_snapshot = true
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  #count = length(var.availability_zones)
+  name       = "rds_sg"
+  #subnet_ids = list(aws_subnet.db_private[count.index].id)
+  subnet_ids = aws_subnet.db_private.*.id
+  tags = {
+    Name = "${var.vpc_name}_rds_sg"
+  }
+}
