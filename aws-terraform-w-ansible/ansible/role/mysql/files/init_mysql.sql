9d82f69f50987cfa64146ae91dca961408557e36
iamjjanga-ouo <namils147@kakao.com>
2021-04-07 20:56:17 +0900

    +ansible: resolve init database problem when database or table or record is exsisting...

--- aws-terraform-w-ansible/ansible/role/mysql/files/init_mysql.sql
+++ aws-terraform-w-ansible/ansible/role/mysql/files/init_mysql.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE `free_board` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `free_board` CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE free_board;
 
-CREATE TABLE `board` (
+CREATE TABLE IF NOT EXISTS `board` (
     `num` int NOT NULL,
     `title` varchar(50) NOT NULL,
     `writer` varchar(50) NOT NULL,
@@ -9,6 +9,18 @@ CREATE TABLE `board` (
     `context` varchar(200) NOT NULL
 );
 
-INSERT INTO `board` (`num`, `title`, `writer`, `views`, `context`) VALUES ('1', 'title_1', 'Hong gil dong', '10', 'Hello');
-INSERT INTO `board` (`num`, `title`, `writer`, `views`, `context`) VALUES ('2', 'title_2', 'Kim Do Hyung', '7', 'This is test!');
-INSERT INTO `board` (`num`, `title`, `writer`, `views`, `context`) VALUES ('3', 'title_333', 'Lee Si hyung', '17', '');
+INSERT INTO `board` (num, title, writer, views, context) 
+    SELECT * FROM (SELECT '1', 'title_1', 'Hong gil dong', '10', 'Hello') AS tmp
+    WHERE NOT EXISTS (
+        SELECT num FROM `board` WHERE num = '1'
+    ) LIMIT 1;
+INSERT INTO `board` (num, title, writer, views, context) 
+    SELECT * FROM (SELECT '2', 'title_2', 'Kim Do Hyung', '7', 'This is test!') AS tmp
+    WHERE NOT EXISTS (
+        SELECT num FROM `board` WHERE num = '2'
+    ) LIMIT 1;
+INSERT INTO `board` (num, title, writer, views, context) 
+    SELECT * FROM (SELECT '3', 'title_333', 'Lee Si hyung', '17', '') AS tmp
+    WHERE NOT EXISTS (
+        SELECT num FROM `board` WHERE num = '3'
+    ) LIMIT 1;
